935d2163d8fbfdedcbfc418f3ffe9cc585f80511
Arthur Caine III <acaine03@gmail.com>
2020-03-21 10:44:45 -0500

    Add more links.

--- store/Links.js
+++ store/Links.js
@@ -1,4 +1,8 @@
 export default [
     { title: "Home", text: "Sign In" },
-    { title: "About Us", text: "About Us" }
+    { title: "Summary", text: "Summary" },
+    { title: "Education", text: "Education" },
+    { title: "Your Vehicle", text: "Your Vehicle" },
+    { title: "About Us", text: "About Us" },
+    { title: "Contact Us", text: "Contact Us" }
 ];
